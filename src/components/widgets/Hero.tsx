4dd780cef263b1a3ea9f05b2ccf9529e08859228
James <jamesandrewpike@gmail.com>
2025-01-12 19:08:16 -0500

    commit

--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -5,58 +5,57 @@ export default component$(() => {
     <div class="relative bg-white/80 dark:bg-gray-900/80 overflow-hidden flex items-center justify-center mx-1.5 mt-1 mb-2 border-radius-dot-25 rounded-lg">
 
 
-    <section class="relative rounded-lg  md:-mt-[76px] not-prose mx-1.5 my-1.5 bg-white/90 dark:bg-gray-900/90 border-radius-dot-25">
+      <section class="relative rounded-lg  md:-mt-[76px] not-prose mx-1.5 my-1.5 bg-white/90 dark:bg-gray-900/90 border-radius-dot-25">
 
 
 
-      <div class="relative max-w-7xl mx-auto px-4 sm:px-10 ">
-        <div class="pt-0 md:pt-[76px] pointer-events-none"></div>
-        <div class="pt-6 md:py-20 lg:py-0 lg:flex lg:items-center lg:h-screen lg:gap-8">
-          <div class="basis-1/2 text-center lg:text-left pb-8 md:pb-16 mx-auto">
-          <a href="/reviews" class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-6 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
-          <span class="text-xs bg-primary rounded-full text-white px-4 py-1.5 mr-3">New</span> <span class="text-sm font-medium">Free Website & SEO Audit!</span> 
-          <svg class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-      </a>
-            <h1 class="text-5xl md:text-7xl font-bold leading-tighter tracking-tighter text-primary mb-4 font-heading dark:text-gray-200">
-            
-              <span class="text-sky-500">Website Design & Development Services</span>
-            </h1>
-            <div class="max-w-3xl mx-auto lg:max-w-none">
-              <p class="text-xl mb-6 dark:text-slate-300">
-              Transform your online presence with our custom web design and development services. We help businesses create stunning, fast, and secure websites.
+        <div class="relative max-w-7xl mx-auto px-4 sm:px-10 ">
+          <div class="pt-0 md:pt-[76px] pointer-events-none"></div>
+          <div class="pt-6 md:py-20 lg:py-0 lg:flex lg:items-center lg:h-screen lg:gap-8">
+            <div class="basis-1/2 text-center lg:text-left pb-8 md:pb-16 mx-auto">
+              <a href="/reviews" class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-6 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
+                <span class="text-xs bg-primary rounded-full text-white px-4 py-1.5 mr-3">New</span> <span class="text-sm font-medium">Free Website & SEO Audit!</span>
+                <svg class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+              </a>
+              <h1 class="text-5xl md:text-7xl font-bold leading-tighter tracking-tighter text-primary mb-4 font-heading dark:text-gray-200">
 
-              </p>
+                <span class="text-sky-500">Website Design & Development Services</span>
+              </h1>
+              <div class="max-w-3xl mx-auto lg:max-w-none">
+                <p class="text-xl mb-6 dark:text-slate-300">
+                  Transform your online presence with our custom web design and development services. We help businesses create stunning, fast, and secure websites.
 
-              <div class=" sm:max-w-md m-auto grid grid-cols-1 border-radius-dot-25 gap-4 sm:grid-cols-2 lg:grid-cols-2 lg:max-w-7xl">
-                <div class="flex w-full">
-                  <button class="btn w-full bg-blue-200 py-1 rounded-sm dark:bg-blue-400">
-                    Get Started
-                  </button>
+                </p>
+
+                <div class=" sm:max-w-md m-auto grid grid-cols-1 border-radius-dot-25 gap-4 sm:grid-cols-2 lg:grid-cols-2 lg:max-w-7xl">
+                  <div class="flex w-full">
+                    <button class="btn w-full bg-blue-200 py-1 rounded-sm dark:bg-blue-400">
+                      Get Started
+                    </button>
+                  </div>
+                  <div class="flex w-full">
+                    <button class="btn w-full bg-red-200 py-1 rounded-sm dark:bg-red-400">
+                      Book A Free Consultation
+                    </button>
+                  </div>
+
                 </div>
-                <div class="flex w-full">
-                  <button class="btn w-full bg-red-200 py-1 rounded-sm dark:bg-red-400">
-                    Book A Free Consultation
-                  </button>
-                </div>
-           
+
               </div>
-
             </div>
-          </div>
 
 
 
 
-          <div class="basis-1/2 ">
-          {/* <object type="image/svg+xml" class="border-radius-dot-25 rounded-md" data="https://cdn.svgator.com/images/2021/10/animated-astronaut-illustration.svg" width="100%"></object> */}
-          <img src="https://cdn.svgator.com/images/2021/10/solar-system-animation.svg" width="700" height="400" fetchPriority="high" alt="Animated Vectors Flying to the Moon and Back: About the Space Animation Trend"/>
+            <div class="basis-1/2 ">
+              
+            </div>
+
           </div>
 
         </div>
 
-      </div>
-
-    </section>
-  </div>
+      </section>
+    </div>
   );
 });
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -29,12 +29,39 @@ export default component$(() => {
         <source src="https://assets.codepen.io/6093409/river.mp4" type="video/mp4"/>
       </video>
       <div class="home-content">
-        <h1>NATURE</h1>
-        <p>EXPERIENCE</p>
+      div class="basis-1/2 text-center lg:text-left pb-8 md:pb-16 mx-auto">
+              <a href="/reviews" class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-6 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
+                <span class="text-xs bg-primary rounded-full text-white px-4 py-1.5 mr-3">New</span> <span class="text-sm font-medium">Free Website & SEO Audit!</span>
+                <svg class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+              </a>
+              <h1 class="text-5xl md:text-7xl font-bold leading-tighter tracking-tighter text-primary mb-4 font-heading dark:text-gray-200">
 
-        <button>EXPLORE THE WORLD</button>
+                <span class="text-sky-500">Website Design & Development Services</span>
+              </h1>
+              <div class="max-w-3xl mx-auto lg:max-w-none">
+                <p class="text-xl mb-6 dark:text-slate-300">
+                  Transform your online presence with our custom web design and development services. We help businesses create stunning, fast, and secure websites.
+
+                </p>
+
+                <div class=" sm:max-w-md m-auto grid grid-cols-1 border-radius-dot-25 gap-4 sm:grid-cols-2 lg:grid-cols-2 lg:max-w-7xl">
+                  <div class="flex w-full">
+                    <button class="btn w-full bg-blue-200 py-1 rounded-sm dark:bg-blue-400">
+                      Get Started
+                    </button>
+                  </div>
+                  <div class="flex w-full">
+                    <button class="btn w-full bg-red-200 py-1 rounded-sm dark:bg-red-400">
+                      Book A Free Consultation
+                    </button>
+                  </div>
+
+                </div>
+
+              </div>
+            </div>
       </div>
-    </div>
+    
       <Hero />
 
       <Features
